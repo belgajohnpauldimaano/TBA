f21d3072cef8b31b8e65626c082cb05df2a358a5
Fernando Sanchez <fernandocsanchez5@gmail.com>
2017-06-14 15:54:00 +0800

    Added class active if categ is selected

--- resources/views/frontend/layouts/film_categ.blade.php
+++ resources/views/frontend/layouts/film_categ.blade.php
@@ -1,13 +1,13 @@
 <section class="film-categ text-center">
     <ul class="film-categ__list list-inline text-uppercase">
         <li class="film-categ__item">
-        	<a class="film-categ__item__link {{ route('films') === 'films' ? 'film-categ__item__link--active' : '' }}" href="{{ route('films') }}">Film List</a>
+        	<a class="film-categ__item__link {{ Request::is('films') ? 'film-categ__item__link--active' : '' }}" href="{{ route('films') }}">Film List</a>
         </li>
         <li class="film-categ__item">
-        	<a class="film-categ__item__link {{ route('trailers') === 'trailers' ? 'film-categ__item__link--active' : '' }}" href="{{ route('trailers') }}" href="">Trailers</a>
+        	<a class="film-categ__item__link {{ Request::is('trailers') ? 'film-categ__item__link--active' : '' }}" href="{{ route('trailers') }}" href="">Trailers</a>
         </li>
         <li class="film-categ__item">
-        	<a class="film-categ__item__link" href="{{ route('on_dvd') }}">On DVD</a>
+        	<a class="film-categ__item__link {{ Request::is('on-dvd') ? 'film-categ__item__link--active' : '' }}" href="{{ route('on_dvd') }}">On DVD</a>
         </li>
     </ul>
 </section>
