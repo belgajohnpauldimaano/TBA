26fed5731a636de6f4c3be4c9a9e114cc4ae487b
Fernando Sanchez <fernandocsanchez5@gmail.com>
2017-06-16 10:22:05 +0800

    Make homepage carousel dynamic

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -3,21 +3,13 @@
 @section('container')
     <div class="hero">
         <div class="hero__owl owl-carousel">
-            <a href="#" class="hero__owl__slide" style="background-image: url({{ asset('frontend/assets/img/hero/1.jpg') }});">
-                <img src="{{ asset('frontend/assets/img/hero/1.jpg') }}" class="w-100">
-            </a>
-            <a href="#" class="hero__owl__slide" style="background-image: url({{ asset('frontend/assets/img/hero/2.jpg') }});">
-                <img src="{{ asset('frontend/assets/img/hero/2.jpg') }}" class="w-100">
-            </a>
-            <a href="#" class="hero__owl__slide" style="background-image: url({{ asset('frontend/assets/img/hero/3.jpg') }});">
-                <img src="{{ asset('frontend/assets/img/hero/3.jpg') }}" class="w-100">
-            </a>
-            <a href="#" class="hero__owl__slide" style="background-image: url({{ asset('frontend/assets/img/hero/4.jpg') }});">
-                <img src="{{ asset('frontend/assets/img/hero/4.jpg') }}" class="w-100">
-            </a>
-            <a href="#" class="hero__owl__slide" style="background-image: url({{ asset('frontend/assets/img/hero/5.jpg') }});">
-                <img src="{{ asset('frontend/assets/img/hero/5.jpg') }}" class="w-100">
-            </a>
+            @if($Carousel)
+                @foreach ($Carousel as $Carousel)
+                    <a href="#" class="hero__owl__slide" style="background-image: url({{ asset('content/carousel/'.$Carousel->image) }});">
+                        <img src="{{ asset('content/carousel/'.$Carousel->image) }}" class="w-100">
+                    </a>
+                @endforeach
+            @endif
         </div>
     </div>
 @endsection
