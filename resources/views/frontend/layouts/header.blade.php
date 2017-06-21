1a2af9c00605d77856435db10a21b00308954866
Fernando Sanchez <fernandocsanchez5@gmail.com>
2017-06-21 14:23:46 +0800

    Minor changes

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -16,7 +16,7 @@
     @yield('styles')
 
     <link href="{{ asset('frontend/assets/css/app.css') }}" / rel="stylesheet">
-    <link href=favicon.jpg rel=icon>
+    <link href="{{ asset('favicon.jpg') }}" rel=icon>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
