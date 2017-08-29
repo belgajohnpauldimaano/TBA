977a9a318d830341634b035933143a9a24e5f911
Fernando Sanchez <fernandocsanchez5@gmail.com>
2017-08-29 11:58:35 +0800

    Comment list blog

--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -55,6 +55,32 @@
                     @endforeach
                 @endif
             </div>
+
+            {{-- @if ($Blog->count() > 0)
+                <div class="row">
+                    <div class="col-md-12">
+                        @foreach ($Blog as $data)
+                            @if (!in_array($data->id, $latest_id))
+                                <div class="media">
+                                    <div class="media-left p-r-4">
+                                        <a href="blog/{{$data->id}}">
+                                            <div class="blog-cover blog-cover--blog m-b-0" style="background-image: url({{ asset('content/film/press_release/'.$data->article_image) }}); height: 200px; width: 200px;"></div>
+                                        </a>
+                                    </div>
+                                    <div class="media-body">
+                                        <h3 class="media-heading m-t-1">{{ str_limit($data->title, $limit = 60, $end = '...') }}</h3>
+                                        <p>{{ date('F d, Y', strtotime($data->created_at)) }}</p>
+                                        <p>{!! str_limit($data->content, $limit = 350, $end = '...') !!}</p>
+                                        <a href="blog/{{$data->id}}" class="btn btn-default p-x-6">Read more</a>
+                                    </div>
+                                </div>
+                                <hr>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            @endif --}}
+            
             {{-- <pre>{{ json_encode($Blog, JSON_PRETTY_PRINT)}}</pre> --}}
         </div>
     </main>
